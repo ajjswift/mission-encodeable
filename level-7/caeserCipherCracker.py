1e2d5917a77041d2f60609a29b8eb4e287e61369
Alex Swift <alex.jones@jellybaby.com>
2024-09-03 18:48:38 +0100

    Level 7 completed

--- level-7/caeserCipherCracker.py
+++ level-7/caeserCipherCracker.py
@@ -1,9 +1,28 @@
-# A program to:
+def decipher_word(word):
+    word = word.upper()
+    shifts = []
 
-# ----------------
-# Subprograms
-# ----------------
+    # Store the shifted versions of the word with corresponding shift values
+    for i in range(-13, 14):  # Shift range from -13 to +13
+        shifted_word = []
+        for letter in word:
+            if 'A' <= letter <= 'Z':  # Only shift alphabetic characters
+                new_char = chr((ord(letter) - 65 + i) % 26 + 65)
+                shifted_word.append(new_char)
+            else:
+                shifted_word.append(letter)
+        shifts.append((i, ''.join(shifted_word)))
 
+    return shifts
+
+def print_shifts(shifts):
+    max_shift_length = max(len(shift[1]) for shift in shifts)
+
+    # Format the output
+    for i, (shift_val, shifted_word) in enumerate(shifts):
+        # Aligning all the shifts
+        shift_output = f"Shift {shift_val:+3}: {shifted_word.center(max_shift_length)}"
+        print(shift_output)
 
 # ----------------
 # Main program
@@ -11,10 +30,5 @@
 
 word = input("Enter a ciphered word: ")
 
-for i in range(27):
-    currentShift = i
-    wordarr = []
-    for letter in word:
-        wordarr.append(chr((ord(letter) - currentShift) % 26 + 65))
-    print(f"CURRENT SHIFT: {i}" + "".join(wordarr))
-    print()
+shifts = decipher_word(word)
+print_shifts(shifts)
